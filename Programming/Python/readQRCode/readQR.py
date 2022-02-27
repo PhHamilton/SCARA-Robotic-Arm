51f7319bb0726068a50837673aa2a76320be1649
Philip <philip.h.hamilton@gmail.com>
2022-02-27 13:04:15 +0100

    Separated the code into multiple functions

--- Programming/Python/readQRCode/readQR.py
+++ Programming/Python/readQRCode/readQR.py
@@ -10,40 +10,66 @@ class QRCalibration():
         
         self.bMsk = (0,0,0)
         self.wMsk = (90,90,90)
+        self.nTries = 10
+        self.points = np.zeros((4,2), np.int32)
         self.img = None
 
-    def getCorners(self, path, DEBUG = False): 
-        self.img = cv2.imread(path)
+    def getImage(self, img, path = None):
+        if path is not None: 
+            self.img = cv2.imread(path)
+        else: 
+            self.img = img#cv2.imread(img)
+
+    def getCorners(self, img, path = None, DEBUG = False): 
+        self.getImage(img, path)
+
         mask = cv2.inRange(self.img,(self.bMsk),(self.wMsk))
         thresholded = cv2.cvtColor(mask,cv2.COLOR_GRAY2BGR)
         inverted = 255-thresholded # black-in-white
+        validPoints = False
 
-        rctPoints = np.zeros((4,2), np.int32)
-        for barCode in decode(inverted):
-            pts = np.array([barCode.polygon], np.int32)
-            pts.reshape((-1,1,2))
-            cv2.polylines(self.img, [pts], True, (255, 0, 0), 10)
-            label = barCode.data.decode('utf-8')
-            # print(pts[0][2])
-            # print(label, pts[0,0])
-            if(label == "Top Left"): 
-                rctPoints[0,0] = pts[0,0][0]
-                rctPoints[0,1] = pts[0,0][1]
-            elif(label == "Bottom Left"):
-                rctPoints[1,0] = pts[0,1][0]
-                rctPoints[1,1] = pts[0,1][1]
-            elif(label == "Top Right"):
-                rctPoints[3,0] = pts[0,3][0]
-                rctPoints[3,1] = pts[0,3][1]
-            elif(label == "Bottom Right"):
-                rctPoints[2,0] = pts[0,2][0]
-                rctPoints[2,1] = pts[0,2][1]
+        for i in range(self.nTries):
+            rctPoints = np.zeros((4,2), np.int32)
+            for barCode in decode(inverted):
+                pts = np.array([barCode.polygon], np.int32)
+                pts.reshape((-1,1,2))
+                cv2.polylines(self.img, [pts], True, (255, 0, 0), 10)
+                label = barCode.data.decode('utf-8')
+                # print(pts[0][2])
+                # print(label, pts[0,0])
+                if(label == "Top Left"): 
+                    rctPoints[0,0] = pts[0,0][0]
+                    rctPoints[0,1] = pts[0,0][1]
+                elif(label == "Bottom Left"):
+                    rctPoints[1,0] = pts[0,1][0]
+                    rctPoints[1,1] = pts[0,1][1]
+                elif(label == "Top Right"):
+                    rctPoints[3,0] = pts[0,3][0]
+                    rctPoints[3,1] = pts[0,3][1]
+                elif(label == "Bottom Right"):
+                    rctPoints[2,0] = pts[0,2][0]
+                    rctPoints[2,1] = pts[0,2][1]
+            if len(rctPoints[:,1]) == 4 and self.validEntries(rctPoints) == True:
+                self.points = rctPoints
+                break
 
         # rectPts.reshape((-1,1,2))
         # print(pts)
         # print(rectPts)
-        cv2.polylines(self.img, [rctPoints], True, (255, 0, 0), 10)
-        return rctPoints
+        # cv2.polylines(self.img, [self.points], True, (255, 0, 0), 10)
+        return self.points
+
+    def drawRectangle(self, img, corners): 
+        cv2.polylines(img, [corners], True, (255, 0, 0), 10)
+
+    def validEntries(self, points):
+        for i in range(4): 
+            # Check that elements are non zero
+            if(points[i, 0] == 0 or points[i, 1] == 0):
+                return False
+            else: 
+                continue
+        return True
 
     def showImage(self): 
         cv2.imshow("Image", self.img)
