7613ab15ae101db9c5f1112b0275958bba9f8acd
Philip <philip.h.hamilton@gmail.com>
2022-03-13 12:51:49 +0100

    Code refractoring

--- Programming/Python/ColorRecognition/lib/ColorRecognition.py
+++ Programming/Python/ColorRecognition/lib/ColorRecognition.py
@@ -29,7 +29,10 @@ class detectColor():
     def findColor(self): 
         if(self.upperBoundSet and self.upperBoundSet): 
             hsvImage = cv2.cvtColor(self.img, cv2.COLOR_BGR2HSV)
-            self.msk = cv2.inRange(hsvImage, self.lowerBound, self.upperBound)
+            try: 
+                self.msk = cv2.inRange(hsvImage, self.lowerBound, self.upperBound)
+            except: 
+                pass
             
             if(self.kernel is not None):
                 self.FilteredImage = self.erodeImage()
@@ -52,27 +55,20 @@ class detectColor():
         yList = []
         for c in contours:
             M = cv2.moments(c)
-            print(M['m00'])
+            # print(M['m00'])
             if(M['m00'] == 0.0): 
                 continue
             xList.append(int(M['m10']/M['m00']))
             yList.append(int(M['m01']/M['m00']))
-
+                
         return xList, yList
 
 
     def drawCross(self, img, x, y, cross_color, line_length, text = None): 
-        for i in range(len(x)):
-            cv2.line(img, (x[i]-line_length, y[i]), (x[i]+line_length, y[i]), cross_color, 5)
-            cv2.line(img, (x[i], y[i]-line_length), (x[i], y[i]+line_length), cross_color, 5)
+            cv2.line(img, (x-line_length, y), (x+line_length, y), cross_color, 5)
+            cv2.line(img, (x, y-line_length), (x, y+line_length), cross_color, 5)
             if(text != None): 
-                cv2.putText(img, text, (x[i] + 20, y[i]+80), cv2.FONT_HERSHEY_COMPLEX, 2, (255,255,255), 2)
-        # for c in filtered_contours:
-        #     M = cv2.moments(c)
-        #     x = int(M['m10']/M['m00'])
-        #     y = int(M['m01']/M['m00'])
-        #     cv2.line(img, (x-line_length, y), (x+line_length, y), cross_color, 2)
-        #     cv2.line(img, (x, y-line_length), (x, y+line_length), cross_color, 2)
+                cv2.putText(img, text, (x + 20, y+80), cv2.FONT_HERSHEY_COMPLEX, 2, (255,255,255), 2)
 
     def erodeImage(self):
         return cv2.erode(self.msk, self.kernel, iterations = 2)
@@ -112,53 +108,3 @@ class detectColor():
     
     def saveImage(self): 
         cv2.imwrite("/Users/Philip/Documents/Programming/SCARA-Robotic-Arm/Programming/Python/Color Recognition/images/FilteredImage.png", self.FilteredImage)
-
-    def convertToPIL(self): 
-        pass
-
-        
-
-
-if __name__ == "__main__":
-    # imgPath = "/Users/Philip/Documents/Programming/SCARA-Robotic-Arm/Programming/Python/Color Recognition/images/colors.jpg"
-    script_dir = sys.path[0]
-    Path = "../images/colors.jpg"
-    imgPath = os.path.join(script_dir, Path)
-    a = detectColor(imgPath)
-
-    if sys.argv[1] == "Blue":
-        a.setName("Blue")
-        a.setLowerBound(90,50,50)
-        a.setUpperBound(130,255,255)
-    elif sys.argv[1] == "Orange":
-        a.setName("Orange")
-        a.setLowerBound(9,0,20)
-        a.setUpperBound(25,255,255)
-
-    elif sys.argv[1] == "Red":
-        a.setName("Red")
-        a.setLowerBound(150,50,20)
-        a.setUpperBound(179,255,255)
-
-    # a.setName("Green")
-    # a.setLowerBound(50,150,20)
-    # a.setUpperBound(70,255,255)
-    a.setKernel(10)
-
-    # For example gimp uses H = 0-360, S = 0-100 and V = 0-100. But OpenCV uses  H: 0-179, S: 0-255, V: 0-255.
-    # https://stackoverflow.com/questions/10948589/choosing-the-correct-upper-and-lower-hsv-boundaries-for-color-detection-withcv
-
-    a.findColor()
-    a.saveImage()
-
-    # filtered = a.process_and_detect("/Users/Philip/Documents/Programming/SCARA-Robotic-Arm/Programming/Python/Color Recognition/images/FilteredImage.png")
-    x,y = a.getCoordinates()
-    
-    a.drawCross(a.img, x,y, (255,0,0), 20)
-
-    # cv2.imshow(a.name, a.img)
-    cv2.imshow(a.name, a.img)
-    cv2.waitKey(0)
-
-    # a.saveImage()
-    # a.showAll()
--- Programming/Python/readQRCode/warpImage.py
+++ Programming/Python/readQRCode/warpImage.py
@@ -15,7 +15,7 @@ def mmToPixel(pos, width):
     return width / QRDistance * pos
 
 def pixelTomm(pos, width): 
-    return QRDistance/width * pos[0]
+    return QRDistance/width * pos
 
 
 class warpImage():
@@ -32,36 +32,6 @@ class warpImage():
         cv2.imshow("TransformedImage", self.warpedImage)
         cv2.waitKey(0)
         
-if __name__ == '__main__':
-    path = r'/Users/Philip/Documents/Programming/SCARA-Robotic-Arm/Programming/Python/readQRCode/img/testWColors.jpg'
-    test = QRCalibration()
-    corners = test.getCorners(path)
-    
-    width = corners[2,0] - corners[0,0]
-
-    # # height = corners[1,1] - corners[0,1]
-
-    warped = warpImage(test.img, width)
-    warped.warpImage(corners)
-
-    setPoint(10, -40, warped.warpedImage, width)
-    test.showImage()
-    warped.showImage()
-    # width = corners[3,0] - corners[0,0]
-    # # height = corners[1,1] - corners[0,1]
-    # corners = np.float32(corners)
-    # pts = np.float32([[0,0], [0, width], [width, width], [width, 0]])
-    
-    # print(corners)
-    # print(pts)
-    # print(width/2)
-    # tMatrix = cv2.getPerspectiveTransform(corners, pts)
-
-    # img2 = cv2.warpPerspective(test.img, tMatrix, (width, width))
-    # cv2.circle(img2, (np.int32(width/2), np.int32(width/2)), 10, (0,0,255),cv2.FILLED)
-    # setPoint(40,50,img2, width)
-    # cv2.imshow("Transormed Image", img2)
-    # test.showImage()
     
 
 
